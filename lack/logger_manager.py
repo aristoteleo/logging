b337e92f0bd1c00ff33fe67ecf2a8d344b92234d
user_name <dummtindex@gmail.com>
2022-01-19 19:29:54 -0500

    make LoggerManager a non purely static class (can be used across pkgs)

--- lack/logger_manager.py
+++ lack/logger_manager.py
@@ -7,31 +7,27 @@ class LoggerManager:
     CRITICAL = logging.CRITICAL
     EXCEPTION = logging.ERROR
 
-    main_logger_name = "dynamo"
-    main_logger = Logger("dynamo")
-    temp_timer_logger = Logger("lack-temp-timer-logger")
-
-    @staticmethod
-    def set_main_logger_name(name: str):
-        LoggerManager.main_logger = Logger(name)
-        LoggerManager.main_logger_name = name
-
-    @staticmethod
-    def get_main_logger():
-        return LoggerManager.main_logger
-
     @staticmethod
     def gen_logger(namespace: str):
         return Logger(namespace)
 
-    @staticmethod
-    def get_temp_timer_logger():
-        return LoggerManager.temp_timer_logger
+    def __init__(self, main_logger_name: str = "lack", temp_timer_logger: str = "lack-temp-timer-logger"):
+        self.set_main_logger_name(main_logger_name)
+        self.temp_timer_logger = Logger(temp_timer_logger)
 
-    @staticmethod
-    def progress_logger(generator, logger=None, progress_name="", indent_level=1):
+    def set_main_logger_name(self, name: str):
+        self.main_logger = Logger(name)
+        self.main_logger_name = name
+
+    def get_main_logger(self):
+        return self.main_logger
+
+    def get_temp_timer_logger(self):
+        return self.temp_timer_logger
+
+    def progress_logger(self, generator, logger=None, progress_name="", indent_level=1):
         if logger is None:
-            logger = LoggerManager.get_temp_timer_logger()
+            logger = self.get_temp_timer_logger()
         iterator = iter(generator)
         logger.log_time()
         i = 0
@@ -47,3 +43,72 @@ class LoggerManager:
                 prev_progress_percent = new_progress_percent
             yield next(iterator)
         logger.finish_progress(progress_name=progress_name, indent_level=indent_level)
+
+    def main_set_level(self, level):
+        set_logger_level(self.main_logger_name, level)
+
+    def main_info(self, message, indent_level=1):
+        self.main_logger.info(message, indent_level)
+
+    def main_debug(self, message, indent_level=1):
+        self.main_logger.debug(message, indent_level)
+
+    def main_warning(self, message, indent_level=1):
+        self.main_logger.warning(message, indent_level)
+
+    def main_exception(self, message, indent_level=1):
+        self.main_logger.exception(message, indent_level)
+
+    def main_critical(self, message, indent_level=1):
+        self.main_logger.critical(message, indent_level)
+
+    def main_tqdm(self, generator, desc="", indent_level=1, logger=None):
+        """a TQDM style wrapper for logging something like a loop.
+        e.g.
+        for item in main_tqdm(alist, desc=""):
+            do something
+
+        Parameters
+        ----------
+        generator : [type]
+            same as what you put in tqdm
+        desc : str, optional
+            description of your progress
+        """
+        if logger is None:
+            logger = self.main_logger
+        return self.progress_logger(generator, logger=logger, progress_name=desc, indent_level=indent_level)
+
+    def main_log_time(
+        self,
+    ):
+        self.main_logger.log_time()
+
+    def main_silence(
+        self,
+    ):
+        self.main_logger.setLevel(logging.CRITICAL + 100)
+
+    def main_finish_progress(self, progress_name=""):
+        self.main_logger.finish_progress(progress_name=progress_name)
+
+    def main_info_insert_adata(self, key, adata_attr="obsm", indent_level=1, *args, **kwargs):
+        self.main_logger.info_insert_adata(key, adata_attr=adata_attr, indent_level=indent_level, *args, **kwargs)
+
+    def main_info_insert_adata_var(self, key, indent_level=1, *args, **kwargs):
+        self.main_info_insert_adata(self, key, "var", indent_level, *args, **kwargs)
+
+    def main_info_insert_adata_uns(self, key, indent_level=1, *args, **kwargs):
+        self.main_info_insert_adata(key, "uns", indent_level, *args, **kwargs)
+
+    def main_info_insert_adata_obsm(self, key, indent_level=1, *args, **kwargs):
+        self.main_info_insert_adata(key, "obsm", indent_level, *args, **kwargs)
+
+    def main_info_insert_adata_obs(self, key, indent_level=1, *args, **kwargs):
+        self.main_info_insert_adata(key, "obs", indent_level, *args, **kwargs)
+
+    def main_info_insert_adata_layer(self, key, indent_level=1, *args, **kwargs):
+        self.main_info_insert_adata(key, "layers", indent_level, *args, **kwargs)
+
+    def main_info_verbose_timeit(self, msg):
+        self.main_logger.info(msg)
